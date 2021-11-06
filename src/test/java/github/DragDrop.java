3da5c40add7d85ef4f847647c7319018fdc9f9a6
AnyaBlin <93152022+AnyaBlin@users.noreply.github.com>
2021-11-06 23:44:26 +0300

    Add files via upload

---
+++ src/test/java/github/DragDrop.java
@@ -0,0 +1,24 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragDrop {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.startMaximized = true;
+
+    }
+    @Test
+    void DragAndDropAct ()
+    {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
+    }
+}
